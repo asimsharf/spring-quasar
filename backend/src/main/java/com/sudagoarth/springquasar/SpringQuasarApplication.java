305254b4d4759d574a819de3fef1d291d5767e27
asimsharf <asimsharf@gmail.com>
2023-04-23 00:40:51 +0300

    first commit

---
+++ backend/src/main/java/com/sudagoarth/springquasar/SpringQuasarApplication.java
@@ -0,0 +1,13 @@
+package com.sudagoarth.springquasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuasarApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sudagoarth/springquasar/SpringQuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudagoarth.springquasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
